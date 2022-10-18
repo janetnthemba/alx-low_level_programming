b07b2c0d8de27d9341908ff10d06f96e9a2ebc91
janetnthemba <janet.nthemba@gmail.com>
2022-10-18 10:24:37 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "putchar.h"
+/**
+ * main - Entry point
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	putchar('-');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
