9afbf3c6c7f324e1d0793c0e2e108332ef89161b
janetnthemba <janet.nthemba@gmail.com>
2022-10-18 10:37:38 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main-program entry point.
+ * Return:0-no error,non zero value if error.
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
