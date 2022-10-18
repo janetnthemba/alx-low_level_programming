d128729057e14238d4d4f609613dc71bbe3a80e0
janetnthemba <janet.nthemba@gmail.com>
2022-10-18 14:06:26 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+#include "6-abs.c"
+
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all numbers from input to 98
+ * @n: the starting number
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+}
+else
+{
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+		if (i != 98)
+		{
+			printf(", ");
+		}
+	}
+}
+	printf("\n");
+}
+
