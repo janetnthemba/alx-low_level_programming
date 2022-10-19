63d61c73511e3edb834170a99935a555325162f2
janetnthemba <janet.nthemba@gmail.com>
2022-10-19 12:01:20 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void print_sum_(void);
+
+/**
+ * print_sum - your message
+ *
+ */
+
+void print_sum(void)
+{
+	int a = 2;
+	int total = 0;
+
+	while (a < 1024)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+			total += i;
+		a += 1;
+	}
+	printf("%d\n", total);
+}
