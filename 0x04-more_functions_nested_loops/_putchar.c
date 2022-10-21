062d5d0b8546152ab5431edd07157660165ef4d0
janetnthemba <janet.nthemba@gmail.com>
2022-10-21 03:27:37 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unstd.h>
+#include "main.h"
+
+/*
+ * _putchar - writes the character c to stdout
+ * @c:The character to print
+ *Return:On sucess 1 else -1
+ */
+int _putchar(char c)
+{
+	Return (write (1, &c, 1));
+}
