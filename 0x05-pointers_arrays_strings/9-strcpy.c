28e4d7a766afd5189753b92c75c039f17c6a232e
janetnthemba <janet.nthemba@gmail.com>
2022-10-25 05:14:03 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,4 @@
-#include "mainh.h"
+#include "main.h"
 #include "2-strlen.c"
 
 /**
@@ -17,5 +17,5 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	return dest;
+	return (dest);
 }
