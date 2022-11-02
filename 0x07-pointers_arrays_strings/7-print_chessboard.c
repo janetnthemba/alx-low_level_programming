2b4ffe17e616a2a618be92f426f7b99713c94417
janetnthemba <janet.nthemba@gmail.com>
2022-11-02 11:35:38 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,19 +7,19 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i = 0;
-	int j;
-
-	while (i < 8)
+	int i, j;
+	
+	for (i = 0; i < 8; i++)
 	{
-		j = 0;
-		while (j < 8)
+		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
 			if (j == 7)
+			{
+				_putchar(a[i][j]);
 				_putchar('\n');
-			j++;
+			}
+			else
+				_putchar(a[i][j]);
 		}
-		i++;
 	}
 }
