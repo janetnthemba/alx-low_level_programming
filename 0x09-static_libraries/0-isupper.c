fbc94bd0ac84d3796f0a2454bfb69d85dae94785
janetnthemba <janet.nthemba@gmail.com>
2022-11-08 03:24:37 -0800

    Static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isupper - checks if uppercase
+ *@c: character to check
+ *Return: Always 0
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isdigit - determine if digit
+ *@c: character to determine
+ * Return: gives boolean
+ */
+
+int _isdigit(int c)
+{
+	int y = 0;
+	
+	if (c >= '0' && c <= '9')
+		y = 1;
+
+	return (y);
+}
